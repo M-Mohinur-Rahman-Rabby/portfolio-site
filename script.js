a34f1412aeae9b6778a91b652560cdcb2dd0da7a
M-Mohinur-Rahman-Rabby <rafirock379@gmail.com>
2025-06-20 18:58:07 +0600

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,6 @@ const modal = document.getElementById('modal');
 const modalPdf = document.getElementById('modal-pdf');
 const closeModalBtn = document.querySelector('.close-modal');
 
-// Open modal with paper PDF
 document.querySelectorAll('.publication-card').forEach(card => {
   card.addEventListener('click', () => {
     const pdfPath = card.querySelector('a').getAttribute('href');
@@ -34,7 +33,6 @@ document.querySelectorAll('.publication-card').forEach(card => {
   });
 });
 
-// Close modal functionality
 closeModalBtn.addEventListener('click', () => {
   modal.style.display = 'none';
   modalPdf.src = ''; // Clear the PDF source when closing
@@ -42,46 +40,6 @@ closeModalBtn.addEventListener('click', () => {
 
 // Close modal if the backdrop is clicked
 modal.addEventListener('click', (e) => {
-  // Typing animation with academic identity text
-const texts = [
-  "Chemical Engineering Student",
-  "Aspiring PhD Candidate",
-  
-];
-let currentTextIndex = 0;
-let charIndex = 0;
-const typingSpeed = 75;
-const erasingSpeed = 40;
-const delayBetweenTexts = 2000;
-const heroTextElement = document.getElementById("hero-text");
-
-function typeText() {
-  if (!heroTextElement) return;
-  if (charIndex < texts[currentTextIndex].length) {
-    heroTextElement.textContent += texts[currentTextIndex].charAt(charIndex);
-    charIndex++;
-    setTimeout(typeText, typingSpeed);
-  } else {
-    setTimeout(eraseText, delayBetweenTexts);
-  }
-}
-
-function eraseText() {
-  if (!heroTextElement) return;
-  if (charIndex > 0) {
-    heroTextElement.textContent = texts[currentTextIndex].substring(0, charIndex - 1);
-    charIndex--;
-    setTimeout(eraseText, erasingSpeed);
-  } else {
-    currentTextIndex = (currentTextIndex + 1) % texts.length;
-    setTimeout(typeText, typingSpeed);
-  }
-}
-
-window.addEventListener("DOMContentLoaded", () => {
-  typeText();
-});
-
   if (e.target === modal) {
     modal.style.display = 'none';
     modalPdf.src = '';
